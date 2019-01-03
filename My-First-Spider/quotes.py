17800522638f7e65f08b255e44a067f5598d1525
Asim Zaidi <asimzaidih@gmail.com>
2019-01-03 13:19:00 -0600

    fix

--- My-First-Spider/quotes.py
+++ My-First-Spider/quotes.py
@@ -1,33 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-# Understand what a sub class does again?
-class MultipleQuotesPaginationSpider(scrapy.Spider):
-    name = "multiple-quotes-pagination"
-    # any url under this top level domain can be scraped.
-    allowed_domains = ["toscrape.com"]
+
+class AuthorsSpider(scrapy.Spider):
+    name = "authors"
     start_urls = ['http://quotes.toscrape.com']
 
-    # this method is where our Spider will extract the data.
-    # to do this, it has to generate one or many dictionaries.
     def parse(self, response):
-        # self.log('I just visited: ' + response.url)
         urls = response.css('div.quote > span > a::attr(href)').extract()
         for url in urls:
             url = response.urljoin(url)
-            yield scrapy.Request(url=url, callback = self.parse_details   ) 
+            yield scrapy.Request(url=url, callback=self.parse_details)
+
         # follow pagination link
         next_page_url = response.css('li.next > a::attr(href)').extract_first()
-        # Next, we find the url to the next page, then create a request for this new page.
-        # this will repeat till there are no more quotes in this website. 
         if next_page_url:
-            # follow pagination link - 
             next_page_url = response.urljoin(next_page_url)
-            # request the new page and parse it
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_details(self, response):
         yield {
             'name': response.css('h3.author-title::text').extract_first(),
-            'birth-date': response.css('span.author-born::date ').extract_first(),
-        } #for every page, parse method will extract the data 
+            'birth_date': response.css('span.author-born-date::text').extract_first(),
+        }
